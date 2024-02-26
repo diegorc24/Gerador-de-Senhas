23e00f91c30d27229bb56610f10d46f85b642c9b
Diego Rodrigues Cardoso <diego.r.cadoso@hotmail.com>
2024-02-26 20:43:19 -0300

    instalando surge

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+import './style.css';
+
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+  copy(displayPasswordEl.innerHTML);
+  alert('Senha copiada!');
+});
